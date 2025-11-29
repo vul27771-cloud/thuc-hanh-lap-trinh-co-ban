5de89534e401c06f7b8468d1c38802b9fd197dbe
vul27771-cloud <vul27771@gmail.com>
2025-11-29 10:07:41 +0700

    Update bai4.8.py

--- bai4.8.py
+++ bai4.8.py
@@ -1,3 +1,8 @@
+print("sinh vien : le hoang vu")
+
+print("ma so sv :245751030110084")
+
+print("#############################")
 ds = input('Nhap day tu: ').split()  
 max_len = max(len(tu) for tu in ds)
 tu_dai_nhat = [tu for tu in ds if len(tu) == max_len]
@@ -5,3 +10,4 @@ tu_dai_nhat = [tu for tu in ds if len(tu) == max_len]
 print('Cac tu dai nhat:')
 for tu in tu_dai_nhat:
     print(tu)
+
