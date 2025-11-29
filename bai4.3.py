6228139fb574c190192f9e7f39215b5de30255a3
vul27771-cloud <vul27771@gmail.com>
2025-11-29 09:59:39 +0700

    Update bai4.3.py

--- bai4.3.py
+++ bai4.3.py
@@ -1,4 +1,10 @@
+print("sinh vien : le hoang vu")
+
+print("ma so sv :245751030110084")
+
+print("#############################")
 S = input('Nhap chuoi: ')
 for ch in S:
     print(ch.upper())
     
+
