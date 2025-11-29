a7a56ecd3a8a44b8fe8799130a467016c459eef9
vul27771-cloud <vul27771@gmail.com>
2025-11-29 10:01:17 +0700

    Update bai4.4.py

--- bai4.4.py
+++ bai4.4.py
@@ -1,4 +1,10 @@
+print("sinh vien : le hoang vu")
+
+print("ma so sv :245751030110084")
+
+print("#############################")
 ds = input('danh sach: ').split()
 print(ds)
 for so in ds:
     print(so)
+
