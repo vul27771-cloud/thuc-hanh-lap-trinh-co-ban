db6011ec0d9d033908c88c04c7dd374c04d96d97
vul27771-cloud <vul27771@gmail.com>
2025-11-29 10:06:51 +0700

    Update bai4.6.py

--- bai4.6.py
+++ bai4.6.py
@@ -1,6 +1,12 @@
+print("sinh vien : le hoang vu")
+
+print("ma so sv :245751030110084")
+
+print("#############################")
 hoten = input('Nhap ho va ten: ').strip()  
 ds = hoten.split()
 ho = ds[0]
 ten = ds[1]
 print('Ho:', ho)
 print('Ten rieng:', ten)
+
