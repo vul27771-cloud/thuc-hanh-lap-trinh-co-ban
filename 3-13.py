292ea1d5131f6271074c43ec50945ddcf87ed4a1
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:24:11 +0700

    Update 3-13.py

--- 3-13.py
+++ 3-13.py
@@ -1,3 +1,7 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 print("Vị trí của chuỗi abc là:", ds.index('abc'))
+
