456dc8fc11b51f239c1beba54593692d6aa329cc
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:04:55 +0700

    Update BAI 2.7.py

--- BAI 2.7.py
+++ BAI 2.7.py
@@ -1,3 +1,6 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 def checkValue(n):
     if n % 2 == 0:
         print("Đây là một số chẵn")
@@ -5,3 +8,4 @@ def checkValue(n):
         print("Đây là một số lẻ")
 
 checkValue(7)
+
