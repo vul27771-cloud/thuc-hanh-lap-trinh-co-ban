f50336cef674b4022c5fe83d56b6dbfdec215ef1
vul27771-cloud <vul27771@gmail.com>
2025-12-06 08:44:30 +0700

    Update cau1.5.py

--- cau1.5.py
+++ cau1.5.py
@@ -1,8 +1,9 @@
-print("Sinh vien: LE TRONG TRUNG")
-print("Ma so SV : 245751030110070")
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
 print("#############################")
 
 n = int(input("Enter A Number--->"));
 while n >=0:
     print (n);
     n = n - 1;
+
