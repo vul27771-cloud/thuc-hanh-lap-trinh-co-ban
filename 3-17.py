31b091457daf435a2009170191c0bf70ffde80c8
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:25:54 +0700

    Update 3-17.py

--- 3-17.py
+++ 3-17.py
@@ -1,5 +1,9 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 n = int(input("Nhập n: "))
 
 for i in range(1, n+1):
     if n % i == 0:
         print(i)
+
