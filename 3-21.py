c8978f716603662164e4ef6fb8429f5145e4f756
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:27:37 +0700

    Update 3-21.py

--- 3-21.py
+++ 3-21.py
@@ -1,3 +1,6 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 s = input("Nhập chuỗi số nhị phân: ")
 lst = s.split(',')
 
@@ -8,3 +11,4 @@ for b in lst:
         result.append(b)
 
 print(",".join(result))
+
