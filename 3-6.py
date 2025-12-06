8a5bf7ed6419bd141e929b5be97dddca89d1f068
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:31:05 +0700

    Update 3-6.py

--- 3-6.py
+++ 3-6.py
@@ -1,5 +1,9 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 hoten = input("Nhập họ tên: ").split()
 ho = hoten[0]
 ten = hoten[1]
 print("Họ:", ho)
 print("Tên:", ten)
+
