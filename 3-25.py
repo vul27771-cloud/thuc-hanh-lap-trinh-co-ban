e0d65e9070aa59bd2e1138d0190bc713cf44479e
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:29:38 +0700

    Update 3-25.py

--- 3-25.py
+++ 3-25.py
@@ -1,3 +1,6 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 s = input("Nhập danh sách số (cách nhau bởi dấu phẩy): ")
 
 nums = [int(x) for x in s.split(',')]
@@ -5,3 +8,4 @@ nums = [int(x) for x in s.split(',')]
 odd = [str(x) for x in nums if x % 2 == 1]
 
 print(",".join(odd))
+
