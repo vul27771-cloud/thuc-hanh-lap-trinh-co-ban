12edffeaabd4586eb756a1855afa2edae2f7aae3
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:21:03 +0700

    Update 3-11.py

--- 3-11.py
+++ 3-11.py
@@ -1,6 +1,10 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.append('abc')      # thêm phần tử vào cuối list
 
 for ch in ds:
     print(ch)
+
