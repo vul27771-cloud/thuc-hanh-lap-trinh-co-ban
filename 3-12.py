4d8783bd6ba1521d9f49f874f276dbca97afd34a
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:23:55 +0700

    Update 3-12.py

--- 3-12.py
+++ 3-12.py
@@ -1,6 +1,10 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.remove('123')      # xóa phần tử có giá trị '123'
 
 for ch in ds:
     print(ch)
+
