2b335ea6da2e01396581f9177f32ef73a99610cd
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:27:07 +0700

    Update 3-2.py

--- 3-2.py
+++ 3-2.py
@@ -1,5 +1,9 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 S=input('nhap chuoi:')
 for ch in S :
     if ch == ' ' or ch =='\t':
         continue
     print(ch)
+
