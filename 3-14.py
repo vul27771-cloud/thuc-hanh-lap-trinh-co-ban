314d89858a479060a87d6c0f356f1de0ac2837e3
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:24:48 +0700

    Update 3-14.py

--- 3-14.py
+++ 3-14.py
@@ -1,6 +1,10 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.sort()     # sắp xếp theo thứ tự từ điển
 
 for ch in ds:
     print(ch)
+
