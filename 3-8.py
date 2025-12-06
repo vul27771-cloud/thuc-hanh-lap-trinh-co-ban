2306b3a0baf74097dd9cc20c4764f5e741e2a9c9
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:31:32 +0700

    Update 3-8.py

--- 3-8.py
+++ 3-8.py
@@ -1,3 +1,6 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 ds = input("Nhập dãy từ: ").split()
 
 maxlen = max(len(t) for t in ds)
@@ -5,3 +8,4 @@ maxlen = max(len(t) for t in ds)
 for t in ds:
     if len(t) == maxlen:
         print(t)
+
