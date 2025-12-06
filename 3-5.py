53a09f33c5645510f425dd37aba1f91419902155
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:30:53 +0700

    Update 3-5.py

--- 3-5.py
+++ 3-5.py
@@ -1,3 +1,7 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 ds=input('danh sach:').split()
 for item in reversed(ds):
     print(item)
+
