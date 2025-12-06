3cf3de568a475f2cfb0073c3b185ea4f5ba30585
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:02:39 +0700

    Update BAI 2.1.py

--- BAI 2.1.py
+++ BAI 2.1.py
@@ -1,3 +1,6 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 def sum(a, b):
     print("sum = " + str(a + b))
 
@@ -6,3 +9,4 @@ sum(4, 5)
 
 # tính tổng 2 số 3, 7
 sum(3, 7)
+
