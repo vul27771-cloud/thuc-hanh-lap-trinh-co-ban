2a78ac4108354f97f188f0c0a3c807f40b37416d
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:30:36 +0700

    Update 3-4.py

--- 3-4.py
+++ 3-4.py
@@ -1,5 +1,9 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 ds=input('danh sách :').split()
 print(ds)
 
 for so in ds:
     print(so)
+
