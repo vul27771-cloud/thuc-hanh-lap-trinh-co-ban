538a613bef7e126385a97d3e69de0007fa028382
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:25:32 +0700

    Update 3-16.py

--- 3-16.py
+++ 3-16.py
@@ -1,5 +1,9 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 s = input("Nhập chuỗi nhị phân: ")
 lst = s.split(',')
 
 for val in lst:
     print(val)
+
