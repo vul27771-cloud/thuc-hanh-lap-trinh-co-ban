f55bb4480a90da2ef5558740eacae7fa3ef68fa8
vul27771-cloud <vul27771@gmail.com>
2025-12-06 09:26:31 +0700

    Update 3-18.py

--- 3-18.py
+++ 3-18.py
@@ -1,3 +1,6 @@
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
+print("#############################")
 n = int(input("Nhập n: "))
 
 fib = [0, 1]
@@ -6,3 +9,4 @@ for i in range(2, n):
     fib.append(fib[-1] + fib[-2])
 
 print(fib[:n])
+
