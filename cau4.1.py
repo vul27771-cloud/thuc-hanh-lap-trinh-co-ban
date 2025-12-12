1f066026140bf91fc31175764b42c8d31f4d99c3
vul27771-cloud <vul27771@gmail.com>
2025-12-12 13:23:34 +0700

    Update cau4.1.py

--- cau4.1.py
+++ cau4.1.py
@@ -1,5 +1,5 @@
-print("Sinh vien: LE TRONG TRUNG")
-print("Ma so SV : 245751030110070")
+print("Sinh vien: LE HOANG VU")
+print("Ma so SV : 245751030110084")
 print("############################")
 ###################################
 
@@ -14,4 +14,5 @@ class Circle:
 
 aCircle = Circle(2)
 print("Diện tích hình tròn là:", aCircle.area())
+
 
